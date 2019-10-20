5418e4dce7691709aca10d488a69482f81417733
huhn <sehe89@gmail.com>
2019-10-20 16:45:58 +0200

    fix frontend

---
+++ frontend copy/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    'rules': {
+        'no-console': 0,
+        'indent': [
+            2,
+            'tab'
+        ],
+        'quotes': [
+            2,
+            'single'
+        ],
+        'linebreak-style': [
+            2,
+            'unix'
+        ],
+        'semi': [
+            2,
+            'always'
+        ]
+    },
+    'extends': ['eslint:recommended', 'plugin:vue/recommended'],
+    'parserOptions': {
+        "parser": "babel-eslint",
+        "sourceType": "module"
+    }
+};
