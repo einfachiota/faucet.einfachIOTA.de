c03fce97baf02f13bcd4a7b609d87625fab8b56c
Thoralf-M <thoralf.mue@gmail.com>
2020-01-08 17:40:58 +0100

    add info, update dependencies

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
     <div class="hero">
-      <h1 class="heading">
-        Das IOTA Devnet Faucet erklärt
-      </h1>
-      <p class="sub-heading">
-        Sende einfach IOTA Devnet Tokens!
-      </p>
+      <h1 class="heading">Das IOTA Devnet Faucet erklärt</h1>
+      <p class="sub-heading">Sende einfach IOTA Devnet Tokens!</p>
     </div>
 
     <div class="section section-background">
       <div class="container">
-        <p />
+        <div>
+          <a
+            target="_blank"
+            href="https://github.com/einfachiota/faucet.einfachIOTA.de"
+            rel="noreferrer"
+          >Dieses Faucet auf Github.</a>
+          <br />
+          <br />Im Hintergrund läuft das
+          <a
+            target="_blank"
+            href="https://github.com/machineeconomy/iota-payment"
+            rel="noreferrer"
+          >iota-payment Module</a>.
+          <br />Nicht mehr benötigte Tokens können unter
+          <h4>Fülle das Faucet</h4>wieder zurückgesendet werden,
+          <br />damit sie dann wieder für andere verfügbar sind.
+        </div>
       </div>
     </div>
   </div>
